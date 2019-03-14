b705b2bc57d16ee9cf8613d71f6528a7cd7aef5c
hugojuradoconekta <hugo.jurado@conekta.com>
2019-03-14 11:29:36 -0600

    Initialized project Net Core and MySQL with Docker

---
+++ NetCoreWebApi-MySQL/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace NetCoreWebApi_MySQL.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Department
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<Student> Student { get; set; }
+    }
+}
