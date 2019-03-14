426715a3c2d95b494f6773455e6dd90df574e8f2
hugojuradoconekta <hugo.jurado@conekta.com>
2019-03-14 12:07:36 -0600

    Add Department Controller

---
+++ NetCoreWebApi-MySQL/Controllers/DepartmentController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreWebApi_MySQL.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCoreWebApi_MySQL.Controllers
+{
+    [Route("/api/[controller]")]
+    public class DepartmentController : InjectedController
+    {
+       public DepartmentController(StudentContext context) : base(context) { }
+
+        // Get department with given id
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetDepartment(int id)
+        {
+            var department = await db.Departments.FindAsync(id);
+            if (department == default(Department)) 
+            {
+                return NotFound();
+            }
+
+            return Ok(department);
+        }
+
+        // Add a department to db
+        [HttpPost]
+        public async Task<IActionResult> AddDepartment([FromBody] Department department)
+        { 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await db.AddAsync(department);
+            await db.SaveChangesAsync();
+            return Ok(department.ID);
+        }
+    }
+
+    public class InjectedController: ControllerBase
+    {
+        protected readonly StudentContext db;
+
+        public InjectedController(StudentContext context)
+        {
+            db = context;
+        }
+    }
+}
---
+++ NetCoreWebApi-MySQL/Models/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreWebApi_MySQL.Models
+{
+    public class StudentContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public StudentContext(DbContextOptions options) : base(options) { }
+    }
+}
